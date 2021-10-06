dda94d6428c7a0a5a8c4c7c450449a090c17efe6
jieguann <60665347+jieguann@users.noreply.github.com>
2021-10-05 22:08:59 -0400

    Add see through script
    Set the Render render Queue to 3002 to make see through

--- Assets/Scripts/SeeThrough.cs
+++ Assets/Scripts/SeeThrough.cs
@@ -7,12 +7,10 @@ public class SeeThrough : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (GetComponent<Renderer>())
+        {
+            GetComponent<Renderer>().material.renderQueue = 3002;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
